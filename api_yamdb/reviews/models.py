604849f9af5234f46ec2c009069b265e49e7cd8d
Александр Копосов <alexkopss@yandex.ru>
2022-06-29 14:59:53 +0300

    bugs are fixed

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from titles.models import SCORE, Title
 from users.models import User
@@ -13,7 +14,7 @@ class Review(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='author'
+        related_name='reviews'
     )
     pub_date = models.DateField(
         auto_now_add=True
@@ -24,20 +25,24 @@ class Review(models.Model):
     score = models.IntegerField(
         'Оценка отзыва',
         choices=SCORE,
+        validators=[
+            MinValueValidator(1, 'Поставьте оценку от 1 до 10'),
+            MaxValueValidator(10, 'Поставьте оценку от 1 до 10')
+        ],
         blank=True
     )
 
-    def __str__(self):
-        return self.text
-
     class Meta:
-        ordering = ['-id']
+        ordering = ['pub_date']
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
         constraints = [
             models.UniqueConstraint(fields=['title', 'author'],
                                     name='uniq_review')
         ]
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
